3bba380856ace30f090cd4418f8e24e85f646635
ducpm <ducpm@mindx.edu.vn>
2020-11-05 07:06:08 +0000

    Merged PR 6283: update bot token
    update bot token
    
    Related work items: #7019

--- telegram.py
+++ telegram.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 
 def post(text, chat_id=-325010757, mode=None):
-  url = "https://api.telegram.org/bot1022949386:AAFJFbfkBTcuabBuQ8nvFkBYFOLm-VOU6iI/sendMessage"
+  url = "https://api.telegram.org/bot1181085197:AAEzkEZF8DNckOZZrxdHzZqh8w_xrnsu-JQ/sendMessage"
   data = {
     "chat_id": chat_id,
     "text": text,
