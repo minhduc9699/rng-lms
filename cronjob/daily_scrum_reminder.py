4b7e3ea2163c7022e72e411da89f52d743b6b6d3
Phạm Đức <minhduc.096.99@gmail.com>
2021-06-23 12:30:17 +0700

    add image to reminder message

--- cronjob/daily_scrum_reminder.py
+++ cronjob/daily_scrum_reminder.py
@@ -5,4 +5,8 @@ sched = BackgroundScheduler(timezone='Asia/Ho_Chi_Minh')
 
 @sched.scheduled_job('cron', day_of_week='1-5', hour='10')
 def run():
-  telegram.post('Its time for scrum guys!', chat_id=-466348665)
+  telegram.post(
+    '''Its time for scrum guys!
+https://memegenerator.net/img/instances/76612783/oh-yes-its-meeting-time-.jpg''',
+    chat_id=-466348665
+  )
