a55e66b7b9b5a1997dcd88c58242cbcc571a46c5
thanhvh <thanhvh@mindx.edu.vn>
2022-10-06 18:34:18 +0700

    init sentry webhooks

--- api/sentry.py
+++ api/sentry.py
@@ -5,6 +5,8 @@ import json
 import tornado.gen
 import telegram
 from prometheus import get_vps_metrics
+from env.config import Config
+
 
 class SentryHandler(tornado.web.RequestHandler):
 
@@ -13,12 +15,13 @@ class SentryHandler(tornado.web.RequestHandler):
   
   @tornado.gen.coroutine
   def post(self):
+    vps_info = Config.get().sentry
     update = Dict(json.loads(self.request.body))
     payload = update["data"]
     telegram.post(f'''{payload["description_title"]}\n{payload["description_text"]}''', -568254725)
-    vps_metrics = get_vps_metrics('45.117.83.246:9100', 'Production')
+    vps_metrics = get_vps_metrics(vps_info['nodeExporter'], vps_info['vpsName'])
     mgs = "{:_<20} {:_<30} {:_<10}\n".format('Label', 'Timestamp', 'Value')
     for metrics in vps_metrics:
       mgs += "{:_<20} {:_<30} {:_<10}\n".format(metrics['label'], metrics['timestamp'], metrics['value'])
-    telegram.post(mgs, -568254725,'html')
+    telegram.post(mgs, vps_info['chatId'],'html')
     self.write({'update': update})
