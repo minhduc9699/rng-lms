be924edd531c6af0a6666f718fa8e1583f67004e
ducpm <ducpm@mindx.edu.vn>
2022-11-20 21:57:22 +0700

    init

--- infrastructure/mysql/main.py
+++ infrastructure/mysql/main.py
@@ -1,7 +1,8 @@
 import pymysql.cursors
 
-connection = pymysql.connect(host='localhost',
-    user='user',
-    password='passwd',
-    database='db',
-    cursorclass=pymysql.cursors.DictCursor)
+def get_connection():
+    return pymysql.connect(host='localhost',
+        user='user',
+        password='passwd',
+        database='db',
+        cursorclass=pymysql.cursors.DictCursor)
--- modules/ETL.py
+++ modules/ETL.py
@@ -2,12 +2,45 @@ import tornado.web
 from addict import Dict
 import json
 from infrastructure.tasks_queues.tasks_queue import TaskQueue
+from infrastructure.mysql.main import get_connection
 from domains.main import ETL
 import tornado.gen
 
+def save_students(students):
+  # students = {
+  #   ... in sql tables
+  # }
+  connection = get_connection()
+  # SQL COMMAND 
+  # Insert into....
+  return
+
+def save_classes(classes):
+  connection = get_connection()
+  # SQL COMMAND 
+  # Insert into...
+  return
+
+def save_headcount(headcount):
+  connection = get_connection()
+  # SQL COMMAND 
+  # Insert into...
+  return
+
+
+
 class ETLHandler(tornado.web.RequestHandler):
 
-  def get(self):
+  def get(self, students):
+    # INPUT
+    # students = {
+    #   'id':
+    #   'name':
+    #   ...
+    # }
+    # Process
+    save_students(students)
+    # OUTPUT
     self.write({"Hello": "ETL"})
   
   @tornado.gen.coroutine
